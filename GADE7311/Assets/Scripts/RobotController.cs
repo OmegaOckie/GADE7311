5cc97c873729da90ea443acb314083b9a2643fb4
Ockert Michiel van Wyk <20109982@vegaconnect.co.za>
2022-04-24 18:54:18 +0200

    1 - Added robot variables

---
+++ GADE7311/Assets/Scripts/RobotController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotController : MonoBehaviour
+{
+
+    public string RoboName = "Terminator";
+    public int RoboMaxHealth = 100;
+    public int RoboCurrentHealth = 100;
+    public int RoboAttackStrength = 15;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
